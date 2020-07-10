f983dd9573c2a3428d4cd50d6b1dd638151e7af7
st-linhnguyen <linh.nguyen@supremetech.vn>
2020-07-10 14:07:14 +0700

    change mongolab uri

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  url: 'mongodb://heroku_pxs0s5jb:79mb22e7itc2to5rfg5nam353q@ds259802.mlab.com:59802/heroku_pxs0s5jb'
+  // url: 'mongodb://heroku_pxs0s5jb:79mb22e7itc2to5rfg5nam353q@ds259802.mlab.com:59802/heroku_pxs0s5jb'
+  url: process.env.MONGOLAB_URI
 };
+// mongodb://heroku_z1fbj8gl:<dbpassword>@ds049538.mlab.com:49538/heroku_z1fbj8gl
