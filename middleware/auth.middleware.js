f87bd20227de42bc3522300538804e8a8fcb7c7c
st-linhnguyen <linh.nguyen@supremetech.vn>
2020-07-16 16:43:16 +0700

    update middleware

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -10,7 +10,7 @@ const accessTokenSecret = config.secret;
  * @param {*} next 
  */
 const isAuth = async (req, res, next) => {
-  const clientToken = req.body.token || req.query.token || req.headers["x-access-token"];
+  const clientToken = req.body.token || req.query.token || req.headers['Authorization'];
 
   if (clientToken) {
     try {
