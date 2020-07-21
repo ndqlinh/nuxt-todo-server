5eaa4f84640742f4fade25035c036801545fabfe
st-linhnguyen <linh.nguyen@supremetech.vn>
2020-07-21 14:52:06 +0700

    Migrate a Sandbox Heroku Add-on to Atlas

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  url: 'mongodb://heroku_pmlt39n5:f6oqvgt7ccnnkf5p9il7jt0qv9@ds259802.mlab.com:59802/heroku_pmlt39n5'
+  // Old url with mLab Adds-on: mongodb://heroku_pmlt39n5:f6oqvgt7ccnnkf5p9il7jt0qv9@ds259802.mlab.com:59802/heroku_pmlt39n5
+  url: 'mongodb+srv://heroku_pmlt39n5@heroku-pmlt39n5.renks.mongodb.net/heroku_pmlt39n5?retryWrites=true&w=majority'
 };
