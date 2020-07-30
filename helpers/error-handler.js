333c8399741c389586b672e6c765cc325ab315a0
st-linhnguyen <linh.nguyen@supremetech.vn>
2020-07-30 08:12:20 +0700

    send error when UnauthorizedError

--- helpers/error-handler.js
+++ helpers/error-handler.js
@@ -18,7 +18,8 @@ function errorHandler(err, req, res, next) {
   if (err.name === 'UnauthorizedError') {
     // jwt authentication error
     return res.status(401).json({
-      message: 'Invalid Token'
+      message: 'Invalid Token',
+      error: err
     });
   }
 
