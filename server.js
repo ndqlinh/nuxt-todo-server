35be4c16c093e9590672804f750b0403f7295a41
tsu-linhnguyen <ndqlinh36@gmail.com>
2022-05-20 18:17:48 +0700

    add cors

--- server.js
+++ server.js
@@ -19,20 +19,7 @@ app.use(bodyParser.json());
 
 // enable CORS - Cross Origin Resource Sharing
 app.use(cors());
-allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-  // intercept OPTIONS method
-  if ('OPTIONS' == req.method) {
-    res.send(200);
-  }
-  else {
-    next();
-  }
-};
-app.use(allowCrossDomain);
+app.options('*', cors());
 
 // Configuring the database
 const dbConfig = require('./config/db.config');
