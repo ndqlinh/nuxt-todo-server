3293a27772e6d4a6ff63660e9ce9394939f4a11a
tsu-linhnguyen <ndqlinh36@gmail.com>
2022-05-23 15:55:23 +0700

    update db name

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ async function main() {
   // Use connect method to connect to the server
   await client.connect();
   console.log('Connected successfully to server');
-  const db = client.db(dbName);
+  const db = client.db('heroku-pmlt39n5');
   const collection = db.collection('documents');
 
   // the following code examples can be pasted here...
