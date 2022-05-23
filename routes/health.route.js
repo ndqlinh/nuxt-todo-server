1bad9deb02c88254a537e404bdaaa965ecd3b204
tsu-linhnguyen <ndqlinh36@gmail.com>
2022-05-23 15:32:47 +0700

    remove unused code

--- routes/health.route.js
+++ routes/health.route.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const healthController = require('../controllers/health.controller');
-const todoController = require('../controllers/todo.controller');
 
-router.get('/check-health', todoController.findAll);
+router.get('/check-health', healthController);
 
 module.exports = router;
