81c1806d15ea2346aa83e55cadf72475a53f3d7a
tsu-linhnguyen <ndqlinh36@gmail.com>
2022-05-23 16:37:22 +0700

    update password

--- config/db.config.js
+++ config/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   // Old url with mLab Adds-on: mongodb://heroku_pmlt39n5:f6oqvgt7ccnnkf5p9il7jt0qv9@ds259802.mlab.com:59802/heroku_pmlt39n5
-  url: 'mongodb+srv://heroku_pmlt39n5:pwd@heroku-pmlt39n5.renks.mongodb.net/heroku_pmlt39n5?retryWrites=true&w=majority'
+  url: 'mongodb+srv://heroku_pmlt39n5:Xwz8WUdKLcIFjcbr@heroku-pmlt39n5.renks.mongodb.net/heroku_pmlt39n5?retryWrites=true&w=majority'
 };
