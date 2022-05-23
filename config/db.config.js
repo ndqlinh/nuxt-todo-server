e336bfdcf74442d56bacfa59125ac8214ca3a1de
tsu-linhnguyen <ndqlinh36@gmail.com>
2022-05-23 16:17:58 +0700

    restore password

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: 'mongodb+srv://heroku_pmlt39n5:heroku_pmlt39n5@heroku-pmlt39n5.renks.mongodb.net/?retryWrites=true&w=majority'
+  url: 'mongodb+srv://heroku_pmlt39n5:pwd@heroku-pmlt39n5.renks.mongodb.net/?retryWrites=true&w=majority'
 };
