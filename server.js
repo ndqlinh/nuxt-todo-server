86d4f9f1fa7c34f8c7e9c822fada1ff4bdaa36e3
tsu-linhnguyen <ndqlinh36@gmail.com>
2022-05-23 10:28:27 +0700

    using port variable

--- server.js
+++ server.js
@@ -58,6 +58,6 @@ app.use('/api/health', healthRoute);
 app.use(errorHandler);
 
 // Listen for requests
-app.listen(process.env.PORT || 3000, '0.0.0.0', () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Node server is listening on port ${port}`);
 });
