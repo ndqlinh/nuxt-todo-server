70ebb5780b27940d592b711a47e96b4eb88957a5
tsu-linhnguyen <ndqlinh36@gmail.com>
2022-05-23 15:53:16 +0700

    try to using like document

--- server.js
+++ server.js
@@ -29,11 +29,23 @@ const client = new MongoClient(dbConfig.url, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1
 });
-client.connect(err => {
-  const collection = client.db('heroku-pmlt39n5').collection('users');
-  // perform actions on the collection object
-  client.close();
-});
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('documents');
+
+  // the following code examples can be pasted here...
+
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
 
 // const mongoose = require('mongoose');
 // mongoose.Promise = global.Promise;
