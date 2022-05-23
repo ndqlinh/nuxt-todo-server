b3e2f64e669549e15a65f4beb9800fc80455d5fb
tsu-linhnguyen <ndqlinh36@gmail.com>
2022-05-23 15:28:38 +0700

    update api url

--- routes/health.route.js
+++ routes/health.route.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const healthController = require('../controllers/health.controller');
 const todoController = require('../controllers/todo.controller');
 
-router.get('/', todoController.findAll);
+router.get('/check-health', todoController.findAll);
 
 module.exports = router;
